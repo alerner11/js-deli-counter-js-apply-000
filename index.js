870821669060cbbacef3e8f5a17cc82012560df9
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-13 17:57:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nowServing(line){
   if (line.length === 0){
     return "There is nobody waiting to be served!";
   }
+  
   var serving = line[0]; // need this variable to preserve array item we're about to remove
   line.shift(); // removes first item in array
   return `Currently serving ${serving}.`;
@@ -14,16 +15,15 @@ function nowServing(line){
 
 function currentLine(line){
   if (line.length === 0){
-    return "The line is currently empty."
-  }
-  var status = "The line is currently:"
-  
-  for (let i = 0; i < line.length - 1; i++)
-  {
-    status = status.concat(` ${i + 1}. ${line[i]},`);
+    return "The line is currently empty.";
   }
   
-  status = status.concat(` ${line.length}. ${line[line.length-1]}`);
+  var status = "The line is currently:"; // if the line is NOT empty
   
+  for (let i = 0; i < line.length - 1; i++){ // iterates through people in line EXCEPT the last person
+    status = status.concat(` ${i + 1}. ${line[i]},`); // adds each person's number (not index!) and name, plus a comma
+  }
+  
+  status = status.concat(` ${line.length}. ${line[line.length-1]}`); // adds the last person without the comma
   return status;  
 }
