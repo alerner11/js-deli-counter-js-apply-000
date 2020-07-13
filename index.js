79a25093eeeeb5f6165aec90f59a6f8999e423ed
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-13 17:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ function takeANumber(line, name)
 {
   var counter = 0;
   
-  for (let i = 0; i < name.length; i++)
-  {
-    katzDeliLine[counter] = name[counter];
-  }
+ 
   
-  return `Welcome, ${name[counter]}. You are number ${katzDeliLine} in line.`
+  return `Welcome, ${name}. You are number ${line} in line.`
 }
