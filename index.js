29c99aaafe2bcbda8ed1560cd79fb2a3787cdf75
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-13 17:16:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function takeANumber(line, name)
 {
-  var counter = 0;
-  line[counter] = name;
-  counter++;
+  line.Append(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
