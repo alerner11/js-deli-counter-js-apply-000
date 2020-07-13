af02e79a206b4d2d12196ba150b1290134e3799e
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-13 17:38:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ function currentLine(katzDeliLine)
   
   var iterator = list.values();
   
-  return `The line is currently: ${list.values()}`;
+  return `The line is currently: ${for (let elements of iterator}`;
 }
